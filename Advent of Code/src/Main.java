b02778d719492d3beeecbaec121044c96b734355
Alicja Dudek <alicjadudek@iMac1-Green.local>
2023-12-10 15:01:04 +0100

    Working on task1

--- Advent of Code/src/Main.java
+++ Advent of Code/src/Main.java
@@ -1,5 +1,35 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.Scanner;
+
+import static java.lang.Character.isDigit;
+
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+    public static void main(String[] args) throws FileNotFoundException {
+        Scanner odczyt = new Scanner(new File("input.txt"));
+        ArrayList<Integer> tablicaInt = new ArrayList<>();
+            while (odczyt.nextLine() != null) {
+                String text = odczyt.nextLine();
+                System.out.println(text);
+                ArrayList<String> tablica = new ArrayList<>();
+                char[] tab = text.toCharArray();
+                for (char ch : tab) {
+                    if (Integer.valueOf(ch) >= 48 && Integer.valueOf(ch) <= 57) {
+                        tablica.add(String.valueOf(ch));
+                    }
+                }
+                String liczba = tablica.get(0) + tablica.get(tablica.size() - 1);
+                tablica.clear();
+                tablica.add(liczba);
+                System.out.println(tablica);
+                tablicaInt.add(Integer.valueOf(tablica.get(0)));
+                System.out.println(tablicaInt);
+            }
+            int suma = 0;
+            for(int i: tablicaInt){
+                suma+=i;
+            }
+        
     }
 }
